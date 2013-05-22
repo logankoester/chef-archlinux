cf632522685bffd16ea8788dd35987572fda9c8d
Logan Koester <logan@logankoester.com>
2013-05-22 04:20:19 -0400

    Enable locales

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,17 +7,32 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Chef's SystemD provider is hardcoded to /bin, which is incorrect on Arch Linux.
 if node['platform'] == 'arch'
+
+  # Chef's SystemD provider is hardcoded to /bin, which is incorrect on Arch Linux.
   link '/bin/systemctl' do
     to '/usr/bin/systemctl'
   end
-end
 
-execute 'Generate locales' do
-  command 'locale-gen'
-end
+  # https://wiki.archlinux.org/index.php/Locale
+  ruby_block 'Enable desired locales' do
+    block do
+      locales = ['en_US.UTF-8 UTF-8', 'en_US ISO-8859-1']
 
-execute 'Set system-wide locale' do
-  command 'localectl set-locale LANG="en_US.UTF-8"'
+      lg = Chef::Util::FileEdit.new('/etc/locale.gen')
+      locales.each do |locale|
+        lg.search_file_replace_line(/^##{locale}$/, locale)
+      end
+      lg.write_file
+    end
+  end
+
+  execute 'Generate locales' do
+    command 'locale-gen'
+  end
+
+  execute 'Set system-wide locale' do
+    command 'localectl set-locale LANG="en_US.UTF-8"'
+  end
+
 end
