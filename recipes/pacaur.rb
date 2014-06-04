1dabf702b9d2a3cda7ac08681db79e6ce4c3013a
Logan Koester <logan@logankoester.com>
2014-06-04 13:42:54 +0000

    Adds perl dependency for pacaur

--- recipes/pacaur.rb
+++ recipes/pacaur.rb
@@ -1,5 +1,5 @@
-# pacaur (AUR helper) and its dependencies
 include_recipe 'pacman'
+package('perl') { action :install }
 package('expac') { action :install }
 package('yajl') { action :install }
 pacman_aur('cower'){ action [:build, :install] }
