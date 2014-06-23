8a4a7f6159a285c6ea1fd0caeaff86b5b5c2da2d
Logan Koester <logan@logankoester.com>
2014-06-23 23:39:51 +0000

    Updates metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 CHANGELOG
 =========
 
+### v0.2.1
+- Adds libxslt to utilities
+
 ### v0.2.0
 - Adds hostname recipe
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'logan@logankoester.com'
 license          'MIT'
 description      'Recipes to manage core system properties for Arch Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'pacman'
 depends 'hostsfile'
