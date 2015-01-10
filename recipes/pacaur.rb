6e5aa427025e7a1107b20845fd50f91c6f992f24
Logan Koester <logan@logankoester.com>
2015-01-10 21:10:54 +0000

    Use skippgpcheck instead of importing key (to the wrong keyring, besides). The default build user 'nobody' has no home directory, so cannot locally sign keys.

--- recipes/pacaur.rb
+++ recipes/pacaur.rb
@@ -3,12 +3,9 @@ package('perl') { action :install }
 package('expac') { action :install }
 package('yajl') { action :install }
 
-bash 'Importing key for cower' do
-  code <<-EOH
-    pacman-key --recv-key 1EB2638FF56C0C53
-    pacman-key --lsign 1EB2638FF56C0C53
-  EOH
+pacman_aur 'cower' do
+  skippgpcheck true
+  action [:build, :install]
 end
 
-pacman_aur('cower'){ action [:build, :install] }
 pacman_aur('pacaur'){ action [:build, :install] }
