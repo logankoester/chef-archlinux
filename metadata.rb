67669bf35619ca8f252c42bb6eaefa1c3b1487c3
Logan Koester <logan@logankoester.com>
2016-05-05 13:37:28 +0000

    Adds source_url and issues_url to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'logan@logankoester.com'
 license          'MIT'
 description      'Recipes to manage core system properties for Arch Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/logankoester/chef-archlinux'
+issues_url 'https://github.com/logankoester/chef-archlinux/issues'
 version '0.9.2'
 supports 'arch'
 
