a6b2e4c87a3498aa28f22c560fac2a59c0cb0e41
Logan Koester <logan@logankoester.com>
2018-03-08 07:12:16 +0700

    Bump 0.9.7

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Recipes to manage core system properties for Arch Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/logankoester/chef-archlinux'
 issues_url 'https://github.com/logankoester/chef-archlinux/issues'
-version '0.9.6'
+version '0.9.7'
 supports 'arch'
 
 depends 'pacman'
